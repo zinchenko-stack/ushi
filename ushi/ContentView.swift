1dad7845c78d30e62bdcbdb351efa92f6e65a92e
Vladimir Zinchenko <ktwski@icloud.com>
2026-06-12 14:03:07 +0300

    Initial Commit

---
+++ ushi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ushi
+//
+//  Created by Vladimir Zincenco on 12.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ushi/ushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ushiApp.swift
+//  ushi
+//
+//  Created by Vladimir Zincenco on 12.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct ushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
